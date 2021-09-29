0987e6feac31681589009037477d52b926d51333
cesar madera <camgcamg11@gmail.com>
2021-09-29 13:02:00 -0500

    done'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = %w[chinese italian japanese french belgian]
+ratings = (1..5).to_a
+5.times do
+  restaurant = Restaurant.new(address: Faker::Address.full_address, name: Faker::Restaurant.name, category: categories.sample )
+  restaurant.save
+  3.times do
+    review = Review.new(content: Faker::Restaurant.review, rating: ratings.sample)
+    review.restaurant = restaurant
+    review.save
+  end
+end
